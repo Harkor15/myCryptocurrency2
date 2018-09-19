1c2765992aa93bf4f06698e8a1e71a7834cedb87
Harkor <krzbigos@gmail.com>
2018-09-19 13:01:35 +0200

    dialog layout created

---
+++ app/src/main/java/harkor/mycryptocurrency/viewmodel/MainViewModel.java
@@ -0,0 +1,16 @@
+package harkor.mycryptocurrency.viewmodel;
+
+public class MainViewModel {
+
+
+    public void addCrypto(){
+
+    }
+    public void refresh(){
+
+    }
+    public void settings(){
+
+    }
+
+}
